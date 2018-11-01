e6e692963a1cb9c13f6e25926dbcba90f428cc97
Dima <dimanaumych222@gmail.com>
2018-11-01 19:29:41 +0200

    fix

--- js/js.js
+++ js/js.js
@@ -1,5 +1,8 @@
 const adminLogin = 'admin';
 const adminPassword = 'm4ngo1zh4ackz0r';
+const INVALID_INPUT = "Доступ запрещен!";
+const VALID_INPUT = 'Добро пожаловать!';
+
 
 const login = prompt("Введите Логин");
 
@@ -9,17 +12,17 @@ if (login === adminLogin ){
   const password = prompt("Введите пароль!");
 
   if (password === adminPassword){
-    alert('Добро пожаловать!');
+    alert(VALID_INPUT);
 
-  } else if(password == null){
+  } else if(password === null){
     alert("Отменено пользователем!");
 
   }else {
-    alert("Доступ запрещен!");
+    alert(INVALID_INPUT);
   }
 
-} else if (login == null){
+} else if (login === null){
   alert("Отменено пользователем!");
 } else {
-    alert("Доступ запрещен!");
+    alert(INVALID_INPUT);
 }
